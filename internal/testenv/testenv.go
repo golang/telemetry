ead61ca9eff70a6f0bbc9bc9e605237e6c1d234c
Hana (Hyang-Ah) Kim <hyangah@gmail.com>
2023-06-01 14:52:24 -0400

    internal/testenv: add NeedsGo helper
    Some tests and packages need the go command.
    For example, go.dev/cl/499255 adds a package that uses `go mod
    download` and some of its tests won't work if the test environment
    doesn't have 'go' (e.g. android, ios, wasi/wasm builders).
    NeedsGo skips the calling tests if 'go' isn't available.
    x/tools and go projects have similar facilities already, but
    this is simpler than golang.org/x/tools/internal/testenv's NeedsTool
    or Go project's src/internal/testenv's MustHaveGoBuild.
    We just check whether `go env GOROOT` returns something convincing.
    
    Change-Id: I96b7e7ddb5a6b7feb02ab02fefd8e14451eecb82
    Reviewed-on: https://go-review.googlesource.com/c/telemetry/+/499919
    TryBot-Result: Gopher Robot <gobot@golang.org>
    Run-TryBot: Hyang-Ah Hana Kim <hyangah@gmail.com>
    Reviewed-by: Peter Weinberger <pjw@google.com>
    Reviewed-by: Jamal Carvalho <jamal@golang.org>

--- internal/testenv/testenv.go
+++ internal/testenv/testenv.go
@@ -7,7 +7,11 @@
 package testenv
 
 import (
+	"bytes"
+	"fmt"
+	"os/exec"
 	"runtime"
+	"sync"
 	"testing"
 )
 
@@ -17,5 +21,34 @@ func NeedsLocalhostNet(t testing.TB) {
 	switch runtime.GOOS {
 	case "js", "wasip1":
 		t.Skipf(`Listening on "localhost" fails on %s; see https://go.dev/issue/59718`, runtime.GOOS)
+	}
+}
+
+var (
+	hasGoOnce sync.Once
+	hasGoErr  error
+)
+
+func hasGo() error {
+	hasGoOnce.Do(func() {
+		cmd := exec.Command("go", "env", "GOROOT")
+		out, err := cmd.Output()
+		if err != nil { // cannot run go.
+			hasGoErr = fmt.Errorf("%v: %v", cmd, err)
+			return
+		}
+		out = bytes.TrimSpace(out)
+		if len(out) == 0 { // unusual, incomplete go installation.
+			hasGoErr = fmt.Errorf("%v: no GOROOT - incomplete go installation", cmd)
+		}
+	})
+	return hasGoErr
+}
+
+// NeedsGo skips t if the current system does not have 'go'  or
+// cannot run them with exec.Command.
+func NeedsGo(t testing.TB) {
+	if err := hasGo(); err != nil {
+		t.Skipf("skipping test: go is not available - %v", err)
 	}
 }
