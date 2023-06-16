09c2e0cd78050ac7267e5623a69b30cf63639062
Jamal Carvalho <jamalcarvalho@google.com>
2023-06-16 13:45:47 +0000

    godev/internal/upload: create internal upload package
    This package provides methods for querying and reading an upload config
    for use in the local viewer to show the user which data is valid for a
    particular config and the upload server to validate a report upload request.
    
    Change-Id: I4a1a87b8dd05db7c807f233c20d808d29f761b59
    Reviewed-on: https://go-review.googlesource.com/c/telemetry/+/503995
    Run-TryBot: Jamal Carvalho <jamal@golang.org>
    Reviewed-by: Hyang-Ah Hana Kim <hyangah@gmail.com>
    TryBot-Result: Gopher Robot <gobot@golang.org>

---
+++ godev/internal/upload/upload_test.go
@@ -0,0 +1,66 @@
+// Copyright 2023 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package upload
+
+import (
+	_ "embed"
+	"testing"
+)
+
+func TestUploadConfig(t *testing.T) {
+	got, err := ReadConfig("testdata/config.json")
+	if err != nil {
+		t.Fatal(err)
+	}
+	wantGOOS := []string{"linux", "darwin"}
+	wantGOARCH := []string{"amd64", "arm64"}
+	wantGoVersion := []string{"go1.20", "go1.20.1"}
+	wantPrograms := []string{"golang.org/x/tools/gopls", "cmd/go"}
+	wantVersions := [][2]string{
+		{"golang.org/x/tools/gopls", "v0.10.1"},
+		{"golang.org/x/tools/gopls", "v0.11.0"},
+	}
+	wantCounters := [][2]string{
+		{"golang.org/x/tools/gopls", "editor:emacs"},
+		{"golang.org/x/tools/gopls", "editor:vim"},
+		{"golang.org/x/tools/gopls", "editor:vscode"},
+		{"golang.org/x/tools/gopls", "editor:other"},
+		{"cmd/go", "go/buildcache/miss:0"},
+		{"cmd/go", "go/buildcache/miss:1"},
+		{"cmd/go", "go/buildcache/miss:10"},
+		{"cmd/go", "go/buildcache/miss:100"},
+	}
+
+	for _, w := range wantGOOS {
+		if !got.HasGOOS(w) {
+			t.Errorf("got.HasGOOS(%s) = false: want true", w)
+		}
+	}
+	for _, w := range wantGOARCH {
+		if !got.HasGOARCH(w) {
+			t.Errorf("got.HasGOARCH(%s) = false: want true", w)
+		}
+	}
+	for _, w := range wantGoVersion {
+		if !got.HasGoVersion(w) {
+			t.Errorf("got.HasGoVersion(%s) = false: want true", w)
+		}
+	}
+	for _, w := range wantPrograms {
+		if !got.HasProgram(w) {
+			t.Errorf("got.HasProgram(%s) = false: want true", w)
+		}
+	}
+	for _, w := range wantVersions {
+		if !got.HasVersion(w[0], w[1]) {
+			t.Errorf("got.HasVersion(%s, %s) = false: want true", w[0], w)
+		}
+	}
+	for _, w := range wantCounters {
+		if !got.HasCounter(w[0], w[1]) {
+			t.Errorf("got.HasCounter(%s, %s) = false: want true", w[0], w[1])
+		}
+	}
+}
