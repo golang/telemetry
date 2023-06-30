d7ee58610321bab8546d89a7cde5e4fecee5b15a
Hana (Hyang-Ah) Kim <hyangah@gmail.com>
2023-06-30 09:20:46 -0400

    cmd/gotelemetry: add 'dump' command
    This command helps inspecting the counter files accumulated in the local
    directory.
    
    It dumps counter files to JSON.
    It always looks in telemetry.LocalDir, and takes additional
    file names on the command line.
    
    Code was taken from the temporary counterdump tool.
    
    Change-Id: I77c587cc6091dd063bd0ddcc3201903b64b9a42f
    Reviewed-on: https://go-review.googlesource.com/c/telemetry/+/507316
    Reviewed-by: Jamal Carvalho <jamal@golang.org>
    Auto-Submit: Hyang-Ah Hana Kim <hyangah@gmail.com>
    TryBot-Result: Gopher Robot <gobot@golang.org>
    Reviewed-by: Peter Weinberger <pjw@google.com>
    Run-TryBot: Hyang-Ah Hana Kim <hyangah@gmail.com>

--- cmd/gotelemetry/main.go
+++ cmd/gotelemetry/main.go
@@ -6,21 +6,28 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
 	"flag"
 	"fmt"
 	"io"
+	"log"
 	"os"
+	"path/filepath"
+	"strings"
 
+	"golang.org/x/telemetry/internal/counter"
 	"golang.org/x/telemetry/internal/telemetry"
 )
 
 func main() {
+	log.SetFlags(0)
 	flag.Parse()
+	flag.Usage = func() { usage(os.Stderr) }
 
 	args := flag.Args()
 	if len(args) == 0 {
-		fmt.Println(telemetry.Mode())
+		printSetting()
 		return
 	}
 	switch cmd := args[0]; cmd {
@@ -30,9 +37,19 @@ func main() {
 			usage(os.Stderr)
 			os.Exit(1)
 		}
+	case "dump":
+		counterDump(args[1:]...)
 	case "help":
 		usage(os.Stdout)
 	}
+}
+
+func printSetting() {
+	fmt.Println(telemetry.Mode())
+	fmt.Println()
+	fmt.Println("modefile: ", telemetry.ModeFile)
+	fmt.Println("localdir: ", telemetry.LocalDir)
+	fmt.Println("uploaddir:", telemetry.UploadDir)
 }
 
 func setMode(args []string) error {
@@ -46,5 +63,38 @@ func usage(w io.Writer) {
 	fmt.Fprintln(w, "Usage:")
 	fmt.Fprintln(w, "\tgotelemetry")
 	fmt.Fprintln(w, "\tgotelemetry set <on|off|local>")
+	fmt.Fprintln(w, "\tgotelemetry dump [file1 file2 ...]")
 	fmt.Fprintln(w, "\tgotelemetry help")
+}
+
+func counterDump(args ...string) {
+	localdir := telemetry.LocalDir
+	fi, err := os.ReadDir(localdir)
+	if err != nil && len(args) == 0 {
+		log.Fatal(err)
+	}
+	for _, f := range fi {
+		args = append(args, filepath.Join(localdir, f.Name()))
+	}
+	for _, file := range args {
+		if !strings.HasSuffix(file, ".count") {
+			log.Printf("%s: not a counter file, skipping", file)
+			continue
+		}
+		data, err := os.ReadFile(file)
+		if err != nil {
+			log.Printf("%v, skipping", err)
+			continue
+		}
+		f, err := counter.Parse(file, data)
+		if err != nil {
+			log.Printf("%v, skipping", err)
+			continue
+		}
+		js, err := json.MarshalIndent(f, "", "\t")
+		if err != nil {
+			log.Printf("%s: failed to print - %v", file, err)
+		}
+		fmt.Printf("-- %v --\n%s\n", file, js)
+	}
 }
