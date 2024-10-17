50079b3109511aef8be443e8a9e0f462620884c7
Robert Findley <rfindley@google.com>
2024-10-16 21:42:53 -0400

    internal/chartconfig: improve package documentation
    The chartconfig package is referenced directly by go.dev/doc/telemetry,
    yet lacked some precision, and had some stale or misleading information.
    Do a pass cleaning this up.
    
    Change-Id: I546682182348e4870eeaf2dc6edeabf8928c00ff
    Reviewed-on: https://go-review.googlesource.com/c/telemetry/+/620695
    Reviewed-by: Alan Donovan <adonovan@google.com>
    LUCI-TryBot-Result: Go LUCI <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>
    Auto-Submit: Robert Findley <rfindley@google.com>

--- internal/chartconfig/chartconfig.go
+++ internal/chartconfig/chartconfig.go
@@ -2,47 +2,86 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// The chartconfig package defines the ChartConfig type, representing telemetry
-// chart configuration, as well as utilities for parsing and validating this
-// configuration.
+// The chartconfig package defines configuration that specifies both the data
+// that is uploaded to telemetry.go.dev, and the resulting charts that are
+// rendered. Each record in [config.txt] defines the collection and aggregation
+// of a group of related counters, in the custom format specified below. We
+// refer to these records as "chart configs" (and represent them with the
+// [ChartConfig] type), based on the principle that telemetry collection should
+// be derived from views of the data that we want to see. For basic counters,
+// this is indeed the case: chart configs correspond 1:1 with charts on
+// telemetry.go.dev. For stack counters, we use the same format, but have no
+// way to render them graphically.
 //
-// Chart configuration defines the set of aggregations active on the telemetry
-// server, and are used to derive which data needs to be uploaded by users.
-// See the original blog post for more details:
+// The format itself is based on the original telemetry blog post, though it
+// has been modified slightly and may be subject to further change:
 //
-//	https://research.swtch.com/telemetry-design#configuration
+// https://research.swtch.com/telemetry-design#configuration
 //
-// The record format defined in this package differs slightly from that of the
-// blog post. This format is still experimental, and subject to change.
+// # Chart records
 //
-// Configuration records consist of fields, comments, and whitespace. A field
-// is defined by a line starting with a valid key, followed immediately by ":",
+// Chart config records consist of fields, comments, and whitespace. A field is
+// defined by a line starting with a valid key, followed immediately by ":",
 // and then a textual value, which cannot include the comment separator '#'.
 //
 // Comments start with '#', and extend to the end of the line.
 //
 // The following keys are supported. Any entry not marked as (optional) must be
-// provided.
+// provided for the record to pass validation.
 //
+//   - counter: an expression for the group of counters this chart aggregates.
+//     More detail on the syntax of this expression is provided below.
 //   - title: the chart title.
 //   - description: (optional) a longer description of the chart.
-//   - issue: a go issue tracker URL proposing the chart configuration.
+//   - issue: a Go issue tracker URL proposing the chart configuration.
 //     Multiple issues may be provided by including additional 'issue:' lines.
-//     All proposals must be in the 'accepted' state.
-//   - type: the chart type: currently only partition, histogram, and stack are
-//     supported.
+//   - type: the chart type. Currently only partition and stack are supported.
 //   - program: the package path of the program for which this chart applies.
-//   - version: (optional) the first version for which this chart applies. Must
-//     be a valid semver value.
-//   - counter: the primary counter this chart illustrates, including buckets
-//     for histogram and partition charts.
+//   - version: (optional) the first program version for which this chart
+//     applies. Must be a valid semver value. If not provided, the chart
+//     applies to all versions.
 //   - depth: (optional) stack counters only; the maximum stack depth to collect
 //   - error: (optional) the desired error rate for this chart, which
 //     determines collection rate
 //
 // Multiple records are separated by "---" lines.
 //
-// For example:
+// # Counter expressions
+//
+// Each record must specify in its 'counter' field either a single counter, or
+// a group of related counters.
+//
+// As described in the [counter documentation], we decompose counter names into
+// two parts, separated by a ':'. The part before the ':' is referred to as the
+// 'chart name', and the part after the ':' is the 'bucket'. So in the example
+// gopls/gotoolchain:auto, gopls/gotoolchain is the chart name, and auto is the
+// bucket name.
+//
+// In order to be grouped into a single chart, counters must share a common
+// chart name, and so we specify the group of counters using the following
+// compact syntax:
+//
+//	chartname:{bucket1,bucket2,bucket3}
+//
+// Which expands to the following group of counters:
+//
+//	chartname:bucket1
+//	chartname:bucket2
+//	chartname:bucket3
+//
+// # Chart types
+//
+// There are two supported chart types for the 'type' field:
+//
+//   - A 'partition' chart is a bar chart with one bar for each related counter.
+//     The value of the bar is the aggregation of all counts for the program
+//     over the applicable time period.
+//   - A 'stack' chart is not a real chart. It just means that we want to
+//     collect the given stack counter or group of stack counters.
+//
+// # Example
+//
+// Here is a fully worked example, including both partition and stack records:
 //
 //	# This config defines an ordinary counter.
 //	counter: gopls/editor:{emacs,vim,vscode,other} # TODO(golang/go#34567): add more editors
@@ -52,8 +91,6 @@
 //	issue: https://go.dev/issue/12345
 //	program: golang.org/x/tools/gopls
 //	version: v1.0.0
-//	version: [v2.0.0, v2.3.4]
-//	version: [v3.0.0, ]
 //
 //	---
 //
@@ -66,6 +103,9 @@
 //	type: stack
 //	program: golang.org/x/tools/gopls
 //	depth: 10
+//
+// [config.txt]: https://go.googlesource.com/telemetry/+/refs/heads/master/internal/chartconfig/config.txt
+// [counter documentation]: https://go.dev/doc/telemetry#counters
 package chartconfig
 
 // A ChartConfig defines the configuration for a single chart/collection on the
